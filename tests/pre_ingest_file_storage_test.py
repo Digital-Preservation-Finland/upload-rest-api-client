0f5e0a8c7b01cda77fd8fcf6ddb0729584dacfcf
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2021-07-01 15:36:59 +0300

    TPASPKT-715 Add test for "directory_files" method

---
+++ tests/pre_ingest_file_storage_test.py
@@ -0,0 +1,77 @@
+"""Unit tests for `pre_ingest_file_storage` module."""
+
+import pytest
+
+from upload_rest_api_client.pre_ingest_file_storage import PreIngestFileStorage
+
+
+@pytest.mark.parametrize(
+    ('target', 'result'),
+    [
+        (
+            '/',
+            [
+                {'parent_directory_identifier': 'foo1',
+                 'identifier': 'foo3',
+                 'checksum': 'bar3',
+                 'path': '/files1'},
+                {'parent_directory_identifier': 'foo2',
+                 'identifier': 'foo4',
+                 'checksum': 'bar4',
+                 'path': '/directory1/files2'}
+            ]
+        ),
+        (
+            '/directory1',
+            [
+                {'parent_directory_identifier': 'foo2',
+                 'identifier': 'foo4',
+                 'checksum': 'bar4',
+                 'path': '/directory1/files2'}
+            ]
+        ),
+    ]
+)
+def test_directory_files(requests_mock, target, result):
+    """Test `directory_files` method.
+
+    :param requests_mock:
+    :param target: target directory
+    :param result: result returned from tested method
+
+    """
+    # Mock upload-rest-api responses as upload-rest-api would contain
+    # two files:
+    #     /file1
+    #     /directory1/file2
+    requests_mock.get('http://localhost/v1/files',
+                      json={"/": ["file1"], "/directory1": ["file2"]})
+
+    requests_mock.get('http://localhost/v1/files/',
+                      json={"directories": ["directory1"],
+                            "files": ["file1"],
+                            "identifier": "foo1"})
+
+    requests_mock.get('http://localhost/v1/files/directory1',
+                      json={"directories": [],
+                            "files": ["file2"],
+                            "identifier": "foo2"})
+
+    requests_mock.get('http://localhost/v1/files/file1',
+                      json={"file_path": "/files1",
+                            "metax_identifier": 'foo3',
+                            "md5": "bar3",
+                            "timestamp": "2021-06-21T12:45:28+00:00"})
+
+    requests_mock.get('http://localhost/v1/files/directory1/file2',
+                      json={"file_path": "/directory1/files2",
+                            "metax_identifier": 'foo4',
+                            "md5": "bar4",
+                            "timestamp": "2021-06-21T12:45:28+00:00"})
+
+    # Test the method for targe directory
+    client = PreIngestFileStorage(False,
+                                  'http://localhost',
+                                  'testuser',
+                                  'password')
+    assert client.directory_files(target) == result
---
+++ upload_rest_api_client/pre_ingest_file_storage.py
@@ -0,0 +1,173 @@
+"""Pre ingest file storage client module."""
+from __future__ import print_function
+import os
+import sys
+import json
+import tarfile
+import zipfile
+import hashlib
+from time import sleep
+
+import requests
+import urllib3
+from requests.auth import HTTPBasicAuth
+from requests.exceptions import HTTPError
+
+
+def _md5_digest(fpath):
+    """Return md5 digest of file fpath.
+
+    :param fpath: path to file to be hashed
+    :returns: digest as a string
+    """
+    md5_hash = hashlib.md5()
+    with open(fpath, "rb") as _file:
+        # read the file in 1MB chunks
+        for chunk in iter(lambda: _file.read(1024 * 1024), b''):
+            md5_hash.update(chunk)
+
+    return md5_hash.hexdigest()
+
+
+class PreIngestFileStorage():
+    """Pre-ingest file storage client."""
+
+    def __init__(self, verify, host, user, password):
+        """Initialize pre-ingest file storage client.
+
+        :param bool verify: Use SSL verification
+        :param host: API url
+        :param user: username
+        :param password: password
+        """
+        self.session = requests.Session()
+        self.session.verify = verify
+        self.session.auth = (HTTPBasicAuth(user, password))
+        self.session.mount(host, requests.adapters.HTTPAdapter(max_retries=5))
+        self.archives_api = f"{host}/v1/archives"
+        self.metadata_api = f"{host}/v1/metadata"
+        self.files_api = f"{host}/v1/files"
+
+    def browse(self, path):
+        """Browse files and directories.
+
+        :param path: File/directory path
+        """
+        response = self.session.get(
+            "{}/{}".format(self.files_api, path.strip('/'))
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def _wait_response(self, response):
+        status = "pending"
+        polling_url = response.json()["polling_url"]
+
+        while status == "pending":
+            sleep(5)
+            print('.', end='', flush=True)
+            response = self.session.get(polling_url)
+            try:
+                response.raise_for_status()
+            except HTTPError:
+                if response.headers["content-type"] == "application/json":
+                    print(json.dumps(response.json(), indent=4))
+                    sys.exit(1)
+                raise
+            status = response.json()['status']
+
+        print()
+        if status == "error":
+            print(json.dumps(response.json(), indent=4))
+            sys.exit(1)
+
+        return response
+
+    def directory_files(self, target_directory):
+        """Fetch file metadata for all files in directory."""
+        # Get list of all files pre-ingest file storage
+        directory_tree = self.session.get(self.files_api).json()
+        all_file_paths = list()
+        for directory, files in directory_tree.items():
+            for file_ in files:
+                all_file_paths.append(os.path.join(directory, file_))
+
+        # List only files in target directory (and subdirectories)
+        directory_file_paths = [path for path in all_file_paths
+                                if path.startswith(target_directory)]
+
+        # Get file metadata for files in directory (and subdirectories)
+        files = list()
+        for file_path in directory_file_paths:
+            file_ = self.session.get(
+                "{}/{}".format(self.files_api, file_path.strip('/'))
+            ).json()
+            parent_directory \
+                = self.session.get(
+                    "{}/{}".format(self.files_api,
+                                   os.path.dirname(file_path).strip('/'))
+                ).json()
+            files.append({
+                "parent_directory_identifier": parent_directory["identifier"],
+                "identifier": file_["metax_identifier"],
+                "checksum": file_["md5"],
+                "path": file_["file_path"]
+            })
+
+        return files
+
+    def upload_archive(self, source, target):
+        """Upload archive to pre-ingest file storage.
+
+        :param source: path to archive on local disk
+        :param target: target directory path in pre-ingest file storage
+        """
+        # Check that the provided file is either a zip or tar archive
+        if not tarfile.is_tarfile(source) and not zipfile.is_zipfile(source):
+            raise ValueError(f"Unsupported file: '{source}'")
+
+        # Upload the package
+        with open(source, "rb") as upload_file:
+            response = self.session.post(
+                self.archives_api,
+                params={'dir': target.strip('/'), 'md5': _md5_digest(source)},
+                data=upload_file,
+            )
+
+            # Print InsecureRequestWarning only once
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+            try:
+                response.raise_for_status()
+            except HTTPError:
+                if response.headers["content-type"] == "application/json":
+                    print(json.dumps(response.json(), indent=4))
+                    sys.exit(1)
+                raise
+
+        if response.status_code == 202:
+            response = self._wait_response(response)
+
+    def generate_directory_metadata(self, target):
+        """Generate metadata for directory.
+
+        :param source: path to archive on local disk
+        :param target: target directory path in pre-ingest file storage
+        """
+        response = self.session.post(
+            # Character "*" is added to url to enable metadata
+            # generation for root directory. See TPASPKT-719 for more
+            # information.
+            "{}/{}*".format(self.metadata_api, target.strip('/'))
+        )
+        try:
+            response.raise_for_status()
+        except HTTPError:
+            if response.headers["content-type"] == "application/json":
+                print(json.dumps(response.json(), indent=4))
+                sys.exit(1)
+            raise
+        if response.status_code == 202:
+            response = self._wait_response(response)
+        return self.session.get("{}/{}".format(self.files_api,
+                                               target.strip('/'))).json()
