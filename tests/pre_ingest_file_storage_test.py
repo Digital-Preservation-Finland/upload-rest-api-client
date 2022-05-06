Digital-Preservation-Finland/upload-rest-api-client

commit 855f7b9917937d629209d009cca1915e34f2d9a9

TPASPKT-897 Refactor client error handling
Previously each client function had the same copy-pasted way to handle
errors by printing the JSON error to the screen and exit the system.
Handling the output and exiting the system is however the CLI's job, so
move those to the CLI code and remove the error handling boilerplate
from the client.

Changes also needlessly complicated file not found -error handling to
use this same generalised system. As this leaves all client functions
with a simple requests.raise_for_status(), use it as a session hook to
do the raise_for_status call automatically on every request and remove
the explicit raise_for_status calls from the client code as well.

upload_rest_api_client/client.py
@@ -3,15 +3,15 @@ from __future__ import print_function
 
 import argparse
 import configparser
+import json
 import os
 import sys
 
 import argcomplete
+from requests.exceptions import HTTPError
 from tabulate import tabulate
 
-from upload_rest_api_client.pre_ingest_file_storage import (
-    PreIngestFileStorage, PreIngestFileNotFoundError
-)
+from upload_rest_api_client.pre_ingest_file_storage import PreIngestFileStorage
 
 
 def _parse_conf_file(conf):
@@ -184,12 +184,7 @@ def _browse(client, args):
     :param args: Browsing arguments
     """
     project = _get_project_name(args=args, client=client)
-
-    try:
-        resource = client.browse(project, args.path)
-    except PreIngestFileNotFoundError as error:
-        print(error)
-        return
+    resource = client.browse(project, args.path)
 
     for key, value in resource.items():
         print(f"{key}:")
@@ -269,7 +264,15 @@ def main(cli_args=None):
     config = _parse_conf_file(args.config)
 
     client = PreIngestFileStorage(verify, config)
-    args.func(client, args)
+
+    try:
+        args.func(client, args)
+    except HTTPError as exc:
+        if exc.response.headers["content-type"] == "application/json":
+            print(f"Error when performing request to {exc.request.url}:")
+            print(json.dumps(exc.response.json(), indent=4))
+            sys.exit(1)
+        raise
 
 
 if __name__ == "__main__":

upload_rest_api_client/pre_ingest_file_storage.py
@@ -2,9 +2,7 @@
 from __future__ import print_function
 
 import hashlib
-import json
 import os
-import sys
 import tarfile
 import warnings
 import zipfile
@@ -13,7 +11,6 @@ from time import sleep
 import requests
 import urllib3
 from requests.auth import HTTPBasicAuth
-from requests.exceptions import HTTPError
 
 
 def _md5_digest(fpath):
@@ -46,13 +43,6 @@ class HTTPBearerAuth(requests.auth.AuthBase):
         return request
 
 
-class PreIngestFileNotFoundError(Exception):
-    """Exception raised when a file cannot be found in the pre-ingest
-    file storage.
-    """
-    pass
-
-
 class PreIngestFileStorage():
     """Pre-ingest file storage client."""
 
@@ -68,6 +58,13 @@ class PreIngestFileStorage():
         self.session = requests.Session()
         self.session.verify = verify
         self.session.mount(host, requests.adapters.HTTPAdapter(max_retries=5))
+
+        # Automatically run 'raise_for_status' for each response
+        def check_status(resp, **_):
+            """Check status for each response"""
+            resp.raise_for_status()
+        self.session.hooks["response"] = [check_status]
+
         self.archives_api = f"{host}/v1/archives"
         self.metadata_api = f"{host}/v1/metadata"
         self.files_api = f"{host}/v1/files"
@@ -102,7 +99,6 @@ class PreIngestFileStorage():
     def get_projects(self):
         """Retrieve dictionary of projects accessible to the user"""
         response = self.session.get(f"{self.users_api}/projects")
-        response.raise_for_status()
         return response.json()["projects"]
 
     def browse(self, project, path):
@@ -118,20 +114,6 @@ class PreIngestFileStorage():
             "{}/{}/{}".format(self.files_api, project, path.strip('/'))
         )
 
-        # Catch the error of trying to browse a file that does not exist,
-        # separating it from page not found errors
-        if response.status_code == 404:
-            try:
-                content = response.json()
-                raise PreIngestFileNotFoundError(content["error"])
-            except json.JSONDecodeError:
-                # upload-rest-api sends error messages in JSON form.
-                # If decoding JSON fails it is some other HTTP error,
-                # so raise HTTPError
-                response.raise_for_status()
-
-        response.raise_for_status()
-
         return response.json()
 
     def _wait_response(self, response):
@@ -142,19 +124,7 @@ class PreIngestFileStorage():
             sleep(5)
             print('.', end='', flush=True)
             response = self.session.get(polling_url)
-            try:
-                response.raise_for_status()
-            except HTTPError:
-                if response.headers["content-type"] == "application/json":
-                    print(json.dumps(response.json(), indent=4))
-                    sys.exit(1)
-                raise
             status = response.json()['status']
-
-        print()
-        if status == "error":
-            print(json.dumps(response.json(), indent=4))
-            sys.exit(1)
 
         return response
 
@@ -221,14 +191,6 @@ class PreIngestFileStorage():
             # Print InsecureRequestWarning only once
             urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-            try:
-                response.raise_for_status()
-            except HTTPError:
-                if response.headers["content-type"] == "application/json":
-                    print(json.dumps(response.json(), indent=4))
-                    sys.exit(1)
-                raise
-
         if response.status_code == 202:
             response = self._wait_response(response)
 
@@ -248,15 +210,10 @@ class PreIngestFileStorage():
         response = self.session.post(
             f"{self.metadata_api}/{project}/{metadata_target}"
         )
-        try:
-            response.raise_for_status()
-        except HTTPError:
-            if response.headers["content-type"] == "application/json":
-                print(json.dumps(response.json(), indent=4))
-                sys.exit(1)
-            raise
+
         if response.status_code == 202:
             response = self._wait_response(response)
+
         return self.session.get(
             f"{self.files_api}/{project}/{target.strip('/')}"
         ).json()
@@ -274,13 +231,6 @@ class PreIngestFileStorage():
         response = self.session.delete(
             f"{self.files_api}/{project}/{path}"
         )
-        try:
-            response.raise_for_status()
-        except HTTPError:
-            if response.headers["content-type"] == "application/json":
-                print(json.dumps(response.json(), indent=4))
-                sys.exit(1)
-            raise
 
         # When deleting directories the metadata of the files is deleted in a
         # pollable task. Wait for the task to finish.

tests/client_test.py
@@ -246,14 +246,16 @@ def test_browsing_nonexistent_file(requests_mock, capsys):
     requests_mock.get(
         f"{API_URL}/files/{project}/{path}",
         json={"status": 404, "error": "File not found"},
+        headers={"content-type": "application/json"},
         status_code=404
     )
 
-    upload_rest_api_client.client.main(
-        ['browse', '--project', project, path]
-    )
-    captured = capsys.readouterr()
-    assert "File not found" in captured.out
+    with pytest.raises(SystemExit):
+        upload_rest_api_client.client.main(
+            ['browse', '--project', project, path]
+        )
+        captured = capsys.readouterr()
+        assert "File not found" in captured.out
 
 
 @pytest.mark.usefixtures("mock_configuration")

tests/pre_ingest_file_storage_test.py
@@ -1,10 +1,9 @@
 """Unit tests for `pre_ingest_file_storage` module."""
 
 import pytest
+from requests.exceptions import HTTPError
 
-from upload_rest_api_client.pre_ingest_file_storage import (
-    PreIngestFileStorage, PreIngestFileNotFoundError
-)
+from upload_rest_api_client.pre_ingest_file_storage import PreIngestFileStorage
 
 
 @pytest.mark.parametrize(
@@ -122,9 +121,8 @@ def test_browsing_nonexistent_file(requests_mock):
         }
     )
 
-    with pytest.raises(PreIngestFileNotFoundError) as error:
+    with pytest.raises(HTTPError):
         client.browse(project, path)
-    assert "File not found" in str(error.value)
 
 
 def test_delete(requests_mock):

commit f7ffa284565d8c7e23ec7c1a2f4ce156c71440ba

TPASPKT-897 Raise error when upload-rest-api task fails
In commit 855f7b9917937d629209d009cca1915e34f2d9a9 few lines displaying
the error response of a failed task were removed from function
_wait_response, thinking that the requests session hook raise_for_status
would pick up any errors. However, upload-rest-api task might fail and
still give a succesful response, meaning that raise_for_status won't
pick up the error.

Create custom error that is raised for failing tasks in the client, and
show the error message in the CLI.

upload_rest_api_client/client.py
@@ -12,7 +12,7 @@ from requests.exceptions import HTTPError
 from tabulate import tabulate
 
 from upload_rest_api_client.pre_ingest_file_storage import (
-    PreIngestFileStorage, PreIngestFileNotFoundError
+    PreIngestFileStorage, PreIngestFileNotFoundError, TaskError
 )
 
 
@@ -279,6 +279,9 @@ def main(cli_args=None):
             print(json.dumps(exc.response.json(), indent=4))
             sys.exit(1)
         raise
+    except TaskError as exc:
+        print(f"Error when polling task {exc.task_id}:")
+        print(json.dumps(exc.data, indent=4))
 
 
 if __name__ == "__main__":

upload_rest_api_client/pre_ingest_file_storage.py
@@ -51,6 +51,13 @@ class PreIngestFileNotFoundError(Exception):
     pass
 
 
+class TaskError(Exception):
+    """Exception raised when a task in pre-ingest file storage fails."""
+    def __init__(self, task_id, data):
+        self.task_id = task_id
+        self.data = data
+
+
 class PreIngestFileStorage():
     """Pre-ingest file storage client."""
 
@@ -144,6 +151,14 @@ class PreIngestFileStorage():
             print('.', end='', flush=True)
             response = self.session.get(polling_url)
             status = response.json()['status']
+
+        if status == "error":
+            # Task with an error is still a succesful request, meaning
+            # that raise_for_status() does not pick it up. Raise custom
+            # error for these situations.
+            task_id = polling_url.strip("/").split("/")[-1]
+            data = response.json()
+            raise TaskError(task_id, data)
 
         return response
 

tests/client_test.py
@@ -557,3 +557,48 @@ def test_delete(requests_mock, capsys):
     captured = capsys.readouterr()
     assert captured.out == (
         "Deleted '/test_path.txt' and all associated metadata.\n")
+
+
+@pytest.mark.usefixtures('mock_configuration')
+def test_delete_task_fail(requests_mock, capsys, monkeypatch):
+    """Test failing backend task when using delete command.
+
+    :param requests_mock: HTTP request mocker
+    :param capsys: captured command output
+    """
+    # Skip sleeping when polling the task
+    monkeypatch.setattr(
+        "upload_rest_api_client.pre_ingest_file_storage.sleep",
+        lambda _: None
+    )
+
+    project = "test_project"
+    path = "test_path"
+    polling_url = f"{API_URL}/tasks/polling_url_id"
+
+    response = {
+        "file_path": "/test_path",
+        "message": "Deleting metadata",
+        "polling_url": polling_url,
+        "status": "pending"
+    }
+
+    requests_mock.delete(
+        f"{API_URL}/files/{project}/{path}",
+        json=response,
+        status_code=202
+    )
+
+    requests_mock.get(
+        polling_url,
+        json={"status": "error", "message": "error message"},
+        status_code=200
+    )
+
+    upload_rest_api_client.client.main(
+        ["delete", "--project", project, f"/{path}"]
+    )
+
+    captured = capsys.readouterr()
+    assert "Error when polling task polling_url_id:" in captured.out
+    assert '"message": "error message"' in captured.out

tests/pre_ingest_file_storage_test.py
@@ -3,7 +3,7 @@
 import pytest
 
 from upload_rest_api_client.pre_ingest_file_storage import (
-    PreIngestFileStorage, PreIngestFileNotFoundError
+    PreIngestFileStorage, PreIngestFileNotFoundError, TaskError
 )
 
 
@@ -161,3 +161,52 @@ def test_delete(requests_mock):
 
     client.delete("test_project", "filepath")
     assert adapter.called
+
+
+def test_failed_delete_task(requests_mock, monkeypatch):
+    """Test that failed pre-ingest file storage delete task raises an error.
+
+    :param requests_mock: HTTP requests mocker
+    """
+    # Skip sleeping when polling the task
+    monkeypatch.setattr(
+        "upload_rest_api_client.pre_ingest_file_storage.sleep",
+        lambda _: None
+    )
+
+    host = "http://localhost"
+    polling_url = f"{host}/polling_url"
+    project = "test_project"
+    path = "test_path"
+
+    requests_mock.delete(
+        f"{host}/v1/files/{project}/{path}",
+        json={
+            "file_path": "/test_path",
+            "message": "Deleting metadata",
+            "polling_url": polling_url,
+            "status": "pending"
+        },
+        status_code=202
+    )
+
+    task_response = {"status": "error", "message": "error message"}
+    requests_mock.get(
+        polling_url,
+        json=task_response,
+        status_code=200
+    )
+
+    client = PreIngestFileStorage(
+        False,
+        {
+            "host": host,
+            "user": "",
+            "password": "",
+            "token": "test_token"
+        }
+    )
+
+    with pytest.raises(TaskError) as error:
+        client.delete(project, path)
+        assert error.response.json() == task_response